3260fc51886a97a7f9a727b9f4d71fb4c5af9fdd
Marc156 <thos.marcel.328@gmail.com>
2025-03-14 10:40:44 +0100

    Add files via upload

---
+++ secrets.h.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+const char* ssid = ‘votre_ssid’ ;
+const char* password = ‘votre_mot_de_passe’ ;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
